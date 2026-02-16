689f7942caba6f45e28b0b718cf331fb7904eb89
Ross Edlin <rossedlin@gmail.com>
2026-02-16 17:42:01 +0000

    Make code a bit cleaner

--- html/index.php
+++ html/index.php
@@ -1,17 +1,24 @@
 <?php
 
 // Variables with numbers
-$age      = 27;
-$price    = 19.99;
-$quantity = 3;
-$total    = $price * $quantity;
+$age   = rand(13, 25);
+$qty   = rand(1, 5);
+$price = 19.99;
 
-// Arrays
-$numbers = array(10, 20, 30, 40);
-$user    = array(
-    'name'  => 'Alex',
-    'score' => 88,
-    'level' => 5
-);
+if ($age < 21) {
+    echo "You are NOT eligible to buy this product.<br/>";
+} else {
+    echo "You are eligible to buy this product.<br/>";
 
-print_r($user);
+    // Adding 20% tax
+    $unit_cost = round($price * 1.20, 2);
+    $total     = round($unit_cost * $qty, 2);
+
+    $user = array(
+        'qty'       => $qty,
+        'unit_cost' => $unit_cost,
+        'total'     => $total
+    );
+
+    print_r($user);
+}
